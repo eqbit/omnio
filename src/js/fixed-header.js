7d30881069d2cdb585e2408fab5e5a46232a9604
eqbit <eqbits@gmail.com>
2020-05-06 10:59:45 +0500

    fixed header

--- src/js/fixed-header.js
+++ src/js/fixed-header.js
@@ -1,18 +1,15 @@
-"use strict";
-
 {
-  var header = document.querySelector('[data-fixed-header]');
-
-  var checkHeader = function checkHeader() {
+  const header = document.querySelector('[data-fixed-header]');
+  
+  const checkHeader = () => {
     if (window.pageYOffset > 150) {
       header.classList.add('header--visible');
     } else {
       header.classList.remove('header--visible');
     }
   };
-
+  
   checkHeader();
+  
   window.addEventListener('scroll', checkHeader);
 }
-"use strict";
-//# sourceMappingURL=index.js.map
